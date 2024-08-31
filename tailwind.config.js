04e63ceee2c1a0dd998cb03ff6e458553bd75c08
Guide Fari <guideg6@gmail.com>
2024-08-31 15:44:48 +0200

    update header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ['content/**/*.md', 'layouts/**/*.html', 'layouts/**/*.svg'],
   theme: {
     extend: {
-      
+      colors: {
+        highlight: '#F5D547',
+        calloutcolor: 'dodgerblue',
+        hovercolor: '#02394A',
+        darkMaincolor: '#5C8001',
+        accentText: '#ACC196',
+        text: '#C8C2B7',
+      },
       typography: {
         DEFAULT: {
           css: {
