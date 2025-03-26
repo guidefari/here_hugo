a950e9a7f5d5c37e34d86daf3048d2faa7e7a83a
Guide Fari <guideg6@gmail.com>
2025-03-26 23:53:20 +0200

    big dub: move typography styles out of tailwind config and into css.
    except that one `!important`. sies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,37 +11,6 @@ module.exports = {
         accentText: '#ACC196',
         text: '#C8C2B7',
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#C8C2B7',
-            a: {
-              color: 'white',
-              '&:hover': {
-                color: 'black',
-                backgroundColor: 'var(--maincolor)',
-              },
-              borderBottom: '3px solid var(--maincolor)',
-              fontWeight: 700,
-              textDecoration: 'none',
-              code: {
-                color: 'var(--hovercolor)',
-              },
-            },
-            '--tw-prose-headings': '#C8C2B7',
-            blockquote: {
-              color: '#CCC7B8',
-              strong: {
-                color: 'white',
-              }
-            },
-            strong: {
-              color: 'white',
-              fontWeight: 700,
-            },
-          },
-        },
-      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
